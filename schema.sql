76e38ad8e9bd553c7f21b3809589e44c42364d53
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-17 21:57:43 +0200

    Add changes

---
+++ schema.sql
@@ -0,0 +1,10 @@
+/* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
+
+CREATE TABLE animals (
+    id INT,
+    name varchar(100),
+    date_of_birth date,
+    escape_attempts 
+
+);
