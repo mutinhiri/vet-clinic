6a03e5541e26d6c4833bcc0b79f162ff5c72b6c6
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-17 22:44:44 +0200

    Add query

--- queries.sql
+++ queries.sql
@@ -3,3 +3,4 @@
 
 select name from animals where name like '%mon'
 select name from animals where date_of_birth between '2016-01-01'and  '2019-12-12'
+select name from animals where neutered = true and escape_attempts < 3
