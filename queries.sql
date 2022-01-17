68ef5d3864cea25c2f7e42757adbdd2db417405f
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-17 22:42:37 +0200

    Add date query

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+
+select name from animals where name like '%mon'
+select name from animals where date_of_birth between '2016-01-01'and  '2019-12-12'
