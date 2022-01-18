9af9cdd437b09ae03c1df517778e8192f4749d5a
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-18 20:51:47 +0200

    Add changes to queries

--- queries.sql
+++ queries.sql
@@ -9,3 +9,12 @@ select name, escape_attempts from animals where weight_kg > 10.5
 select * from animals where neutered = true
 select * from animals where name <> 'Gabumon'
 select * from animals where weight_kg >= 10.4 and weight_kg <= 17.3
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon%';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
