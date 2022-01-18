6c19f58b58fe8ec65a561055049f04b6802249e9
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-18 21:42:59 +0200

    query average escape attempts

--- queries.sql
+++ queries.sql
@@ -48,3 +48,4 @@ select count(*) from animals where escape_attempts = 0
 select avg(weight_kg) from animals
 select neutered, avg(escape_attempts) from animals group by neutered;
 select species, min(weight_kg), max(weight_kg) from animals group by species
+select species, avg(escape_attempts) from animals where date_of_birth between '1990-01-01' and '2000-12-31' group by species
