a35a8c175d514f7a40bbce77b18c1934a1f8784a
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-18 21:25:19 +0200

    Add rollback to savepoint and commit

--- queries.sql
+++ queries.sql
@@ -30,3 +30,13 @@ rollback
 begin;
 delete  from animals
 rollback
+
+select * from animals
+
+begin;
+delete from animals where date_of_birth > '2022-01-01';
+savepoint sp1
+update animals set weight_kg = weight_kg * -1
+rollback to sp1
+update animals set weight_kg = weight_kg * -1 where weight_kg < 0;
+commit
