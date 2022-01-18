4f849aa65ca6e929ec88b69545daa7de2adb999b
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-18 19:48:24 +0200

    Add query Phantom

--- data.sql
+++ data.sql
@@ -4,3 +4,6 @@ insert into animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
 insert into animals (name, date_of_birth, weight_kg, neutered, escape_attempts) values ('Gabumon', '2018-11-15', 8.0, true, 2)
 insert into animals (name, date_of_birth, weight_kg, neutered, escape_attempts) values ('Pikachu', '2021-01-07', 15.04, false, 1)
 insert into animals (name, date_of_birth, weight_kg, neutered, escape_attempts) values ('Devimon', '2017-05-12', 11.0, true, 5)
+
+insert into animals (name, date_of_birth, weight_kg, neutered, escape_attempts) values ('Charmander', '2020-02-08', -11.0, false, 0)
+insert into animals (name, date_of_birth, weight_kg, neutered, escape_attempts) values ('Plantmon', '2022-11-15', -5.7, true, 2)
--- schema.sql
+++ schema.sql
@@ -7,6 +7,7 @@ CREATE TABLE animals (
     date_of_birth date,
     escape_attempts int,
     neutered boolean,
-    weight_kg real
+    weight_kg real,
+    species varchar
 
 );
