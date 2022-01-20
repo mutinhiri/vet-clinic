905507b4fc12a072b9f69f997447af78943ce0e3
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-20 22:46:10 +0200

    Add queries for answering questions

--- queries.sql
+++ queries.sql
@@ -61,3 +61,6 @@ select species.name, count(*) from animals full outer join species on species.id
 select animals.name, owners.full_name from animals join owners on owners.id = animals.owner_id join species on species.id = animals.species_id where owners.full_name = 'Jennifer Orwell' and species.name = 'Digimon'
 select animals.name, animals.escape_attempts, full_name from animals right join owners on owners.id = animals.owner_id where escape_attempts = 0 and full_name = 'Dean Winchester'
 select owners.full_name, count(animals.owner_id) from animals full join owners on animals.owner_id = owners.id group by owners.id order by count desc limit 1
+
+--queries to answer questrions
+
