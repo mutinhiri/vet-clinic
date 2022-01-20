79b239719b7741a3ba341f9e6346e5d2fe4c5ef9
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-20 22:18:53 +0200

    Agumon visited stephanie Mendez

--- data.sql
+++ data.sql
@@ -44,3 +44,8 @@ insert into vets(name, age, date_of_graduation) values ('Jack Harness', 38, '200
 insert into specializations(vet_id, species_id) values ((select id from vets where name = 'William Tatcher'), (select id from species where name = 'Pokemon' 
 insert into specializations(vet_id, species_id) values ((select id from vets where name = 'Stephanie Mendez'), (select id from species where name = 'Pokemon' and name = 'Digimon' ))
 insert into specializations(vet_id, species_id) values ((select id from vets where name = 'Jack Harness'), (select id from species where name = 'Digimon' ))
+
+--visits querries
+
+insert into visits (animal_id, vet_id, date_of_visit) values ((select id from animals where name = 'Augmon'), (select id from vets where name ='William Tatcher'), 2020-05-24)
+insert into visits (animal_id, vet_id, date_of_visit) values ((select id from animals where name = 'Augmon'), (select id from vets where name ='Stephanie Mendez'), 2020-07-22)
