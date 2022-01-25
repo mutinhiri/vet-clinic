d3d0086e045a8c3760147d41cfd9b266473b6d22
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-25 20:32:10 +0200

    add new table for treatments

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,11 +1,14 @@
 create table patients(
 id int primary key,
 date_of_birth date
-)
+);
 
 create table medical_histories(
   id int primary key,
   admitted_at timestamp,
   patient_id int  references patients(id)
-)
+);
 
+create table treatments(
+  id int primary key,
+)
